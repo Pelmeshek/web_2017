ee1c11e5655e03c92c4ebaa37972202c03737e2c
Pelmeshek <winindex@yandex.ru>
2017-12-21 05:54:13 +0300

    task4

---
+++ task4/solve.go
@@ -0,0 +1,51 @@
+package main
+import "strings"
+
+func RemoveEven(input []int) (result []int) {
+  i := 0
+  result = make([]int, 0)
+  for i < len(input) {
+    if b[i] % 2==1 {
+      result = append(result, input[i])
+    }
+    i++
+  }
+  return
+}
+
+func PowerGenerator(a int) (func() int) {
+  deg := 1;
+  deg = deg * a
+  result = deg
+  return
+}
+
+func Prin(x rune) bool {
+  if x >= 'a' && x <= 'z' {
+    return false
+  } else {
+    return true
+  }
+}
+
+func DifferentWordsCount(c string) (counter int) {
+  dif := strings.ToLower(c)
+  text:= strings.FieldsFunc(dif, Prin)
+  counter = 0;
+  i:= 0;
+  for i < len(d) {
+    if text[i] != "1" {
+      counter++
+      j:=i+1
+      for j < len (text) {
+        if text[i] == text[j] {
+          text[j] = "1"
+        }
+        j++
+      }
+      text[i] = "1"
+    }
+    i++
+  }
+  return
+}
